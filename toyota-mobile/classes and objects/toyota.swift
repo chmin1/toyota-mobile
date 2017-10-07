7870942b3ef8f7914828e8fa803226a632a08b53
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-10-07 16:25:20 -0700

    Working on API

---
+++ toyota-mobile/classes and objects/toyota.swift
@@ -0,0 +1,35 @@
+//
+//  toyota.swift
+//  toyota-mobile
+//
+//  Created by Chavane Minto on 10/7/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class toyota: NSObject {
+    
+    class func createCamry() {
+        
+        let camry = PFObject(className: "camry")
+        
+        camry["image"] = getPFFileFromImage(image: #imageLiteral(resourceName: "MY18_Camry_US_L_01J9_002"))
+        camry["name"] = "2018 Camry"
+        
+        
+    }
+    
+    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
+        // check if image is not nil
+        if let image = image {
+            // get image data and check if that is not nil
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
+    
+}
