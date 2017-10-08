673f1c9d4d7e342279f6d78d59be27bce6157287
Marissa Bush <marissabush@Marissas-MacBook-Pro.local>
2017-10-07 19:35:03 -0700

    started the colors swift

---
+++ toyota-mobile/classes and objects/Colors.swift
@@ -0,0 +1,31 @@
+//
+//  Colors.swift
+//  toyota-mobile
+//
+//  Created by Marissa Bush on 10/7/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorsDict: NSDictionary {
+
+    
+    static var colorsDict = [
+        "redCamryLE": #imageLiteral(resourceName: "MY18_Camry_US_LE_03T3_002"),
+        "blueCamryLE": #imageLiteral(resourceName: "MY18_Camry_US_LE_08W7_002"),
+        "whiteCamryLE": #imageLiteral(resourceName: "MY18_Camry_US_LE_0040_002"),
+        "greyCamryLE": #imageLiteral(resourceName: "MY18_Camry_US_LE_01J9_002"),
+        "blackCamryLE": #imageLiteral(resourceName: "MY18_Camry_US_LE_0218_002"),
+        "whiteCamryL": #imageLiteral(resourceName: "MY18_Camry_US_L_0040_002"),
+        "greyCamryL": #imageLiteral(resourceName: "MY18_Camry_US_L_01J9_002"),
+        "blackCamryL": #imageLiteral(resourceName: "MY18_Camry_US_L_0218_002"),
+        "redCamrySE": #imageLiteral(resourceName: "MY18_Camry_US_SE_03T3_002"),
+        "greyCamrySE": #imageLiteral(resourceName: "MY18_Camry_US_SE_01J9_002"),
+        "blackCamrySE": #imageLiteral(resourceName: "MY18_Camry_US_SE_0218_002"),
+        "redCamryXSE": #imageLiteral(resourceName: "MY18_Camry_US_XSE_03T3_002"),
+        "whiteCamryXSE": #imageLiteral(resourceName: "MY18_Camry_US_XSE_0089_002"),
+        
+    ]
+}
--- toyota-mobile/classes and objects/toyota.swift
+++ toyota-mobile/classes and objects/toyota.swift
@@ -15,7 +15,7 @@ class toyota: NSObject {
         
         let camry = PFObject(className: "camry")
         
-        camry["image"] = getPFFileFromImage(image: #imageLiteral(resourceName: "MY18_Camry_US_L_01J9_002"))
+        camry["image"] = getPFFileFromImage(image: #imageLiteral(resourceName: "MY18_Camry_US_L_01H1_002"))
         camry["name"] = "2018 Camry"
         
         
